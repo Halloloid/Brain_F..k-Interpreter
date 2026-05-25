d58c028ad7a0f6b87a0b0edabec0ce540e74f323
Halloloid <animation12321567@gmail.com>
2026-05-25 22:00:39 +0530

    Implemented ><+-.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{stdin};
+
+// enum commands {
+//     Rightptr, // >
+//     Leftptr,  // <
+//     Inc,      // +
+//     Dec,      // -
+//     Output,   // .
+//     Input,    // ,
+//     LoopStart,// [
+//     LoopEnd,  // ]
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut encoded = String::new();
+    // encoded.push_str("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
+    stdin().read_line(&mut encoded).expect("Failed to Read the Line");
+
+    let mut ptr = 0;
+    let mut tape : Vec<u32> = Vec::new();
+    tape.push(0);
+    for i in encoded.chars(){
+        match i {
+            '>' => {
+                ptr+=1;
+                tape.push(0);
+            },
+            '<' => ptr-=1,
+            '+' => tape[ptr] += 1,
+            '-' => tape[ptr] -= 1,
+            '.' => print!("{}",std::char::from_u32(tape[ptr]).unwrap()),
+            _ => (),
+        } 
+    }
 }
